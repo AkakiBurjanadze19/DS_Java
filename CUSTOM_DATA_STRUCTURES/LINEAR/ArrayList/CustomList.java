b09ed1976ea6904572d974159dea233fcfba053f
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-03 21:21:21 +0400

    add forEach abstract method

--- CUSTOM_DATA_STRUCTURES/LINEAR/ArrayList/CustomList.java
+++ CUSTOM_DATA_STRUCTURES/LINEAR/ArrayList/CustomList.java
@@ -1,8 +1,11 @@
 package CUSTOM_DATA_STRUCTURES.LINEAR.ArrayList;
 
+import java.util.function.Consumer;
+
 public interface CustomList<T> {
     void add(T item);
     void print();
+    void forEach(Consumer<T> action);
     int size();
     boolean isEmpty();
     boolean contains(T item);

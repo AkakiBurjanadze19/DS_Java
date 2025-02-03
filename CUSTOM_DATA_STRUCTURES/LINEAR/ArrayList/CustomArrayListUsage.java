f422b0558c5f3fa6cc63f4cea545dc0c2bd99c90
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-03 21:21:30 +0400

    test forEach method

--- CUSTOM_DATA_STRUCTURES/LINEAR/ArrayList/CustomArrayListUsage.java
+++ CUSTOM_DATA_STRUCTURES/LINEAR/ArrayList/CustomArrayListUsage.java
@@ -1,24 +1,35 @@
 package CUSTOM_DATA_STRUCTURES.LINEAR.ArrayList;
 
+import java.util.function.Consumer;
+
 public class CustomArrayListUsage {
     public static void main(String[] args) {
-        CustomArrayList<String> names = new CustomArrayList<>();
+//        CustomArrayList<String> names = new CustomArrayList<>();
+//
+//        names.add("Akaki");
+//        names.add("Luka");
+//        names.add("Goga");
+//        names.add("Alex");
+//
+//        CustomArrayList<String> clonedNames = names.cloneList();
+//
+//        names.print();
+//        System.out.println();
+//        clonedNames.print();
+//
+//        System.out.println();
+//
+//        System.out.println(names.contains("Goga"));
+//        System.out.println(names.contains("Akaki"));
+//        System.out.println(names.contains("Mathew"));
 
-        names.add("Akaki");
-        names.add("Luka");
-        names.add("Goga");
-        names.add("Alex");
+        CustomArrayList<Integer> numbers = new CustomArrayList<>();
 
-        CustomArrayList<String> clonedNames = names.cloneList();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
 
-        names.print();
-        System.out.println();
-        clonedNames.print();
-
-        System.out.println();
-
-        System.out.println(names.contains("Goga"));
-        System.out.println(names.contains("Akaki"));
-        System.out.println(names.contains("Mathew"));
+        Consumer<Integer> printNum = num -> System.out.println(num);
+        numbers.forEach(printNum);
     }
 }

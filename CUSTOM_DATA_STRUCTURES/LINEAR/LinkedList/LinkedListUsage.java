5d25f6298ab6b654f0c0cff575f3b965f0915749
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-04 20:43:10 +0400

    test new methods

--- CUSTOM_DATA_STRUCTURES/LINEAR/LinkedList/LinkedListUsage.java
+++ CUSTOM_DATA_STRUCTURES/LINEAR/LinkedList/LinkedListUsage.java
@@ -8,7 +8,9 @@ public class LinkedListUsage {
         linkedList.addFirst(20);
         linkedList.addFirst(10);
         linkedList.addLast(40);
+        linkedList.addLast(40);
         linkedList.addLast(50);
+        linkedList.addLast(60);
         linkedList.addLast(60);
 
         System.out.println("size: " + linkedList.size());
@@ -22,5 +24,18 @@ public class LinkedListUsage {
         System.out.println("contains 60: " + linkedList.contains(60));
         System.out.println("contains 10: " + linkedList.contains(10));
         System.out.println("contains 100: " + linkedList.contains(100));
+
+        System.out.println();
+
+//        linkedList.forEach((item) -> System.out.print(item + " "));
+
+        System.out.println(linkedList.get(2));
+
+        System.out.println();
+
+        System.out.println("index of: 60: " + linkedList.indexOf(60));
+        System.out.println("index of 40: " + linkedList.indexOf(40));
+        System.out.println("last index of 60: " + linkedList.lastIndexOf(60));
+        System.out.println("last index of 40: " + linkedList.lastIndexOf(40));
     }
 }

1b86e95d73e43667e1203c015435d1fa48698e63
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-04 19:42:51 +0400

    add other utility methods for removing, setting values, e.t.c

--- CUSTOM_DATA_STRUCTURES/LINEAR/ArrayList/CustomList.java
+++ CUSTOM_DATA_STRUCTURES/LINEAR/ArrayList/CustomList.java
@@ -1,13 +1,23 @@
 package CUSTOM_DATA_STRUCTURES.LINEAR.ArrayList;
 
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public interface CustomList<T> {
     void add(T item);
     void print();
     void forEach(Consumer<T> action);
+    void remove(T item);
+    void remove(int index);
+    void removeAll(CustomArrayList<T> items);
+    void removeIf(Predicate<T> condition);
+    void set(int index, T item);
     int size();
+    int indexOf(T item);
+    int lastIndexOf(T item);
     boolean isEmpty();
     boolean contains(T item);
     CustomArrayList<T> cloneList();
+    T get(int index);
+    Object[] toArray();
 }

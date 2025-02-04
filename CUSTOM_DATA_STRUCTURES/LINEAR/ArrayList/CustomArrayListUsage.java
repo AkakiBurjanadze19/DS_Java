1eb1f9967dfcdb3131da0f67470c82a2d4a6ee94
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-04 19:43:00 +0400

    test new methods

--- CUSTOM_DATA_STRUCTURES/LINEAR/ArrayList/CustomArrayListUsage.java
+++ CUSTOM_DATA_STRUCTURES/LINEAR/ArrayList/CustomArrayListUsage.java
@@ -1,7 +1,5 @@
 package CUSTOM_DATA_STRUCTURES.LINEAR.ArrayList;
 
-import java.util.function.Consumer;
-
 public class CustomArrayListUsage {
     public static void main(String[] args) {
 //        CustomArrayList<String> names = new CustomArrayList<>();
@@ -28,8 +26,39 @@ public class CustomArrayListUsage {
         numbers.add(10);
         numbers.add(20);
         numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
+        numbers.add(40);
 
-        Consumer<Integer> printNum = num -> System.out.println(num);
-        numbers.forEach(printNum);
+//        Consumer<Integer> printNum = num -> System.out.println(num);
+//        numbers.forEach(printNum);
+
+//        System.out.println(numbers.get(0));
+//        System.out.println(numbers.get(numbers.size() - 1));
+//
+//        System.out.println(numbers.indexOf(40));
+//        System.out.println(numbers.indexOf(70));
+//        System.out.println(numbers.lastIndexOf(40));
+
+//        Integer item = 20;
+//        numbers.remove(item);
+//        numbers.remove(1);
+//        numbers.remove(0);
+//        numbers.print();
+
+//        numbers.print();
+//        numbers.removeAll(numbers);
+//        numbers.print();
+
+//        System.out.println(Arrays.toString(numbers.toArray()));
+
+//        numbers.removeAll(numbers);
+//        System.out.println(numbers.size());
+//        numbers.print();
+
+//        Predicate<Integer> isEven = num -> num % 2 == 0;
+//        numbers.removeIf(isEven);
+//        System.out.println(numbers.size());
+//        numbers.print();
     }
 }

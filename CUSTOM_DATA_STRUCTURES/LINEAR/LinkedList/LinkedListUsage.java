7bd57085c7c59367d7cfc22c003611730a37a73c
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-04 23:15:00 +0400

    test new utility methods

--- CUSTOM_DATA_STRUCTURES/LINEAR/LinkedList/LinkedListUsage.java
+++ CUSTOM_DATA_STRUCTURES/LINEAR/LinkedList/LinkedListUsage.java
@@ -1,5 +1,7 @@
 package CUSTOM_DATA_STRUCTURES.LINEAR.LinkedList;
 
+import java.util.Arrays;
+
 public class LinkedListUsage {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList = new LinkedList<>();
@@ -37,5 +39,43 @@ public class LinkedListUsage {
         System.out.println("index of 40: " + linkedList.indexOf(40));
         System.out.println("last index of 60: " + linkedList.lastIndexOf(60));
         System.out.println("last index of 40: " + linkedList.lastIndexOf(40));
+
+        System.out.println();
+
+        linkedList.remove(2);
+        Integer num = 60;
+        linkedList.remove(num);
+
+        linkedList.print();
+
+        linkedList.set(1, 100);
+        linkedList.print();
+
+//        linkedList.removeFirst();
+//        linkedList.print();
+//        linkedList.removeFirst();
+//        linkedList.print();
+//        linkedList.removeFirst();
+//        linkedList.print();
+//        linkedList.removeFirst();
+//        linkedList.print();
+//        linkedList.removeFirst();
+//        linkedList.print();
+//        linkedList.removeFirst();
+//        System.out.println(linkedList.size());
+
+//        linkedList.removeLast();
+//        linkedList.print();
+//        linkedList.removeLast();
+//        linkedList.print();
+
+        System.out.println(Arrays.toString(linkedList.toArray()));
+//        System.out.println(Arrays.toString(cloned.toArray()));
+
+        System.out.println(linkedList.removeFirst());
+        System.out.println(linkedList.removeLast());
+
+        System.out.println("first: " + linkedList.getFirst());
+        System.out.println("element: " + linkedList.element());
     }
 }

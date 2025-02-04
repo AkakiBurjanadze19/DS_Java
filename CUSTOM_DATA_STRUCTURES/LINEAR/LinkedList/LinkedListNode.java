1585ff8d065606e993e7d823d9534200de5e4224
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-04 23:14:49 +0400

    make all properties public

--- CUSTOM_DATA_STRUCTURES/LINEAR/LinkedList/LinkedListNode.java
+++ CUSTOM_DATA_STRUCTURES/LINEAR/LinkedList/LinkedListNode.java
@@ -1,15 +1,11 @@
 package CUSTOM_DATA_STRUCTURES.LINEAR.LinkedList;
 
 public class LinkedListNode<T> {
-    private T value;
+    public T value;
     public LinkedListNode<T> next;
 
     public LinkedListNode(T value) {
         this.value = value;
         this.next = null;
-    }
-
-    public T getValue() {
-        return this.value;
     }
 }

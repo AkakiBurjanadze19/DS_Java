44479df5050733694ce6baaf9927abf68f885196
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-05 21:15:15 +0400

    add HashMap.java

---
+++ CUSTOM_DATA_STRUCTURES/LINEAR/HashMap/HashMap.java
@@ -0,0 +1,137 @@
+package CUSTOM_DATA_STRUCTURES.LINEAR.HashMap;
+
+import java.util.*;
+
+public class HashMap<K, V> {
+    private List<Entry> items;
+    private int size;
+
+    public HashMap() {
+        this.items = new ArrayList<>();
+        this.size = 0;
+    }
+
+    public void put(K key, V value) {
+        if (!this.containsKey(key)) {
+            this.items.add(new Entry(key, value));
+            this.size++;
+        }
+    }
+
+    public V get(K key) {
+        if (this.isEmpty()) {
+            throw new IllegalStateException("Map is empty. Element cannot be retrieved!");
+        }
+
+        for (Entry entry : this.items) {
+            if (entry.getKey().equals(key)) {
+                return (V) entry.getValue();
+            }
+        }
+
+        return null;
+    }
+
+    public boolean containsKey(K key) {
+        for (Entry entry : this.items) {
+            if (entry.getKey().equals(key)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean containsValue(V value) {
+        for (Entry entry : this.items) {
+            if (entry.getValue().equals(value)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean remove(K key) {
+        if (this.isEmpty()) {
+            throw new IllegalStateException("Map is empty. Element cannot be removed!");
+        }
+
+        for (int i = 0; i < this.items.size(); i++) {
+            Entry entry = this.items.get(i);
+            if (entry.getKey().equals(key)) {
+                this.items.remove(i);
+                this.size--;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean remove(K key, V value) {
+        if (this.isEmpty()) {
+            throw new IllegalStateException("Map is empty. Element cannot be removed!");
+        }
+
+        for (int i = 0; i < this.items.size(); i++) {
+            Entry entry = this.items.get(i);
+            if (entry.getKey().equals(key) && entry.getValue().equals(value)) {
+                this.items.remove(i);
+                this.size--;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean replace(K key, V oldValue, V newValue) {
+        for (Entry entry : this.items) {
+            if (entry.getKey().equals(key) && entry.getValue().equals(oldValue)) {
+                entry.setValue(newValue);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public List<V> values() {
+        List<V> valuesList = new ArrayList<>();
+
+        for (Entry entry : this.items) {
+            valuesList.add((V) entry.getValue());
+        }
+
+        return valuesList;
+    }
+
+    public Set<K> keySet() {
+        Set<K> keys = new HashSet<>();
+
+        for (Entry entry : this.items) {
+            keys.add((K) entry.getKey());
+        }
+
+        return Collections.unmodifiableSet(keys);
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+
+        for (Entry entry : this.items) {
+            output.append(entry);
+        }
+
+        return output.toString();
+    }
+}

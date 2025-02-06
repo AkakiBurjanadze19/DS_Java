8999834a02de19752baa9685d9c5e0e43a3f8f6d
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-06 23:29:25 +0400

    add PriorityQueue.java

---
+++ CUSTOM_DATA_STRUCTURES/NON_LINEAR/PriorityQueue/PriorityQueue.java
@@ -0,0 +1,94 @@
+package CUSTOM_DATA_STRUCTURES.NON_LINEAR.PriorityQueue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PriorityQueue {
+    private List<Integer> heap;
+
+    public PriorityQueue() {
+        this.heap = new ArrayList<>();
+    }
+
+    public void offer(int value) {
+        this.heap.add(value);
+        this.bubbleUp(this.size() - 1);
+    }
+
+    public int poll() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException("PriorityQueue is empty!");
+        }
+
+        int minElement = this.heap.get(0);
+        int lastElement = this.heap.remove(this.size() - 1);
+
+        if (!this.isEmpty()) {
+            this.heap.set(0, lastElement);
+            this.bubbleDown(0);
+        }
+
+        return minElement;
+    }
+
+    public int peek() {
+        if (this.isEmpty()) {
+            throw new IllegalStateException("Priority Queue is empty!");
+        }
+
+        return this.heap.get(0);
+    }
+
+    public int size() {
+        return this.heap.size();
+    }
+
+    public boolean isEmpty() {
+        return this.heap.isEmpty();
+    }
+
+    private void bubbleUp(int index) {
+        while (index > 0) {
+            int parentElementIndex = (index - 1) / 2;
+            int currentElement = this.heap.get(index);
+            int parentElement = this.heap.get(parentElementIndex);
+
+            if (currentElement >= parentElement) {
+                break;
+            }
+
+            this.swap(index, parentElementIndex);
+            index = parentElementIndex;
+        }
+    }
+
+    private void swap(int i, int j) {
+        int temp = this.heap.get(i);
+        this.heap.set(i, this.heap.get(j));
+        this.heap.set(j, temp);
+    }
+
+    private void bubbleDown(int index) {
+        int size = this.size();
+        while (true) {
+            int leftChildIndex = 2 * index + 1;
+            int rightChildIndex = 2 * index + 2;
+            int minElementIndex = index;
+
+            if (leftChildIndex < size && this.heap.get(leftChildIndex) < this.heap.get(minElementIndex)) {
+                minElementIndex = leftChildIndex;
+            }
+
+            if (rightChildIndex < size && this.heap.get(rightChildIndex) < this.heap.get(minElementIndex)) {
+                minElementIndex = rightChildIndex;
+            }
+
+            if (minElementIndex == index) {
+                break;
+            }
+
+            this.swap(index, minElementIndex);
+            index = minElementIndex;
+        }
+    }
+}

bc50e1fb8b1677604c7522394b3d818a05c1e265
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-06 18:46:16 +0400

    add comments about time and space complexity

--- CUSTOM_DATA_STRUCTURES/NON_LINEAR/Heap/MaxHeap/MaxHeap.java
+++ CUSTOM_DATA_STRUCTURES/NON_LINEAR/Heap/MaxHeap/MaxHeap.java
@@ -10,11 +10,19 @@ public class MaxHeap {
         this.heap = new ArrayList<>();
     }
 
+    /*
+        Time Complexity: O(log n)
+        Space Complexity: O(1)
+    */
     public void insert(int value) {
         this.heap.add(value);
         this.bubbleUp(this.size() - 1);
     }
 
+    /*
+        Time Complexity: O(log n)
+        Space Complexity: O(1)
+    */
     public int extractMax() {
         if (this.isEmpty()) {
             throw new IllegalStateException("Heap is empty!");
@@ -31,6 +39,10 @@ public class MaxHeap {
         return maxElement;
     }
 
+    /*
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+    */
     public int getMax() {
         if (this.isEmpty()) {
             throw new IllegalStateException("Heap is empty!");
@@ -39,14 +51,26 @@ public class MaxHeap {
         return this.heap.get(0);
     }
 
+    /*
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+    */
     public int size() {
         return this.heap.size();
     }
 
+    /*
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+    */
     public boolean isEmpty() {
         return this.heap.isEmpty();
     }
 
+    /*
+        Time Complexity: O(log n)
+        Space Complexity: O(1)
+    */
     private void bubbleUp(int index) {
         while (index > 0) {
             int parentElementIndex = (index - 1) / 2;
@@ -62,12 +86,20 @@ public class MaxHeap {
         }
     }
 
+    /*
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+    */
     private void swap(int i, int j) {
         int temp = this.heap.get(i);
         this.heap.set(i, this.heap.get(j));
         this.heap.set(j, temp);
     }
 
+    /*
+        Time Complexity: O(log n)
+        Space Complexity: O(1)
+    */
     private void bubbleDown(int index) {
         int size = this.size();
         while (true) {

d4b7035172f619fd5dc4da05ed76305a2cad4ca8
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-07 00:44:38 +0400

    add BinaryTreeNode.java

---
+++ CUSTOM_DATA_STRUCTURES/NON_LINEAR/BinaryTree/BinaryTreeNode.java
@@ -0,0 +1,13 @@
+package CUSTOM_DATA_STRUCTURES.NON_LINEAR.BinaryTree;
+
+public class BinaryTreeNode {
+    public int data;
+    public BinaryTreeNode left;
+    public BinaryTreeNode right;
+
+    public BinaryTreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}

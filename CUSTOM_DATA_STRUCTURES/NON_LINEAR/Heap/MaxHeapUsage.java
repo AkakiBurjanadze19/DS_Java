7f3687fef375a03ee414c4dff6fefd7d7cf7721d
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-06 18:18:50 +0400

    add MaxHeapUsage.java

---
+++ CUSTOM_DATA_STRUCTURES/NON_LINEAR/Heap/MaxHeapUsage.java
@@ -0,0 +1,23 @@
+package CUSTOM_DATA_STRUCTURES.NON_LINEAR.Heap;
+
+import CUSTOM_DATA_STRUCTURES.NON_LINEAR.Heap.MaxHeap.MaxHeap;
+
+public class MaxHeapUsage {
+    public static void main(String[] args) {
+        MaxHeap heap = new MaxHeap();
+
+        heap.insert(5);
+        heap.insert(10);
+        heap.insert(15);
+        heap.insert(20);
+
+//        System.out.println("heap size: " + heap.size());
+//        System.out.println("extracted max: " + heap.extractMax());
+//        System.out.println("heap size: " + heap.size());
+//        System.out.println("max element: " + heap.getMax());
+
+        while (!heap.isEmpty()) {
+            System.out.println(heap.extractMax());
+        }
+    }
+}

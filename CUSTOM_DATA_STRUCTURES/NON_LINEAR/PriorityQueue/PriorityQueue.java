3084d150af2a41c6c1e9fdbdd539f4a03947b5c1
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-06 23:56:38 +0400

    incorporate PriorityQueueNode

--- CUSTOM_DATA_STRUCTURES/NON_LINEAR/PriorityQueue/PriorityQueue.java
+++ CUSTOM_DATA_STRUCTURES/NON_LINEAR/PriorityQueue/PriorityQueue.java
@@ -4,80 +4,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PriorityQueue {
-    private List<Integer> heap;
+    private List<PriorityQueueNode> heap;
 
     public PriorityQueue() {
         this.heap = new ArrayList<>();
     }
 
-    /*
-        Time Complexity: O(log n)
-        Space Complexity: O(1)
-    */
-    public void offer(int value) {
-        this.heap.add(value);
+    public void offer(String value, int priority) {
+        this.heap.add(new PriorityQueueNode(value, priority));
         this.bubbleUp(this.size() - 1);
     }
 
-    /*
-        Time Complexity: O(log n)
-        Space Complexity: O(1)
-    */
-    public int poll() {
+    public String poll() {
         if (this.isEmpty()) {
             throw new IllegalStateException("PriorityQueue is empty!");
         }
 
-        int minElement = this.heap.get(0);
-        int lastElement = this.heap.remove(this.size() - 1);
+        PriorityQueueNode minElement = this.heap.get(0);
+        PriorityQueueNode lastElement = this.heap.remove(this.size() - 1);
 
         if (!this.isEmpty()) {
             this.heap.set(0, lastElement);
             this.bubbleDown(0);
         }
 
-        return minElement;
+        return minElement.getValue();
     }
 
-    /*
-        Time Complexity: O(1)
-        Space Complexity: O(1)
-    */
-    public int peek() {
+    public String peek() {
         if (this.isEmpty()) {
-            throw new IllegalStateException("Priority Queue is empty!");
+            throw new IllegalStateException("PriorityQueue is empty!");
         }
 
-        return this.heap.get(0);
+        return this.heap.get(0).getValue();
     }
 
-    /*
-        Time Complexity: O(1)
-        Space Complexity: O(1)
-    */
     public int size() {
         return this.heap.size();
     }
 
-    /*
-        Time Complexity: O(1)
-        Space Complexity: O(1)
-    */
     public boolean isEmpty() {
         return this.heap.isEmpty();
     }
 
-    /*
-        Time Complexity: O(log n)
-        Space Complexity: O(1)
-    */
     private void bubbleUp(int index) {
         while (index > 0) {
             int parentElementIndex = (index - 1) / 2;
-            int currentElement = this.heap.get(index);
-            int parentElement = this.heap.get(parentElementIndex);
+            PriorityQueueNode currentElement = this.heap.get(index);
+            PriorityQueueNode parentElement = this.heap.get(parentElementIndex);
 
-            if (currentElement >= parentElement) {
+            if (currentElement.getPriority() >= parentElement.getPriority()) {
                 break;
             }
 
@@ -86,20 +62,12 @@ public class PriorityQueue {
         }
     }
 
-    /*
-        Time Complexity: O(1)
-        Space Complexity: O(1)
-    */
     private void swap(int i, int j) {
-        int temp = this.heap.get(i);
+        PriorityQueueNode temp = this.heap.get(i);
         this.heap.set(i, this.heap.get(j));
         this.heap.set(j, temp);
     }
 
-    /*
-        Time Complexity: O(log n)
-        Space Complexity: O(1)
-    */
     private void bubbleDown(int index) {
         int size = this.size();
         while (true) {
@@ -107,11 +75,11 @@ public class PriorityQueue {
             int rightChildIndex = 2 * index + 2;
             int minElementIndex = index;
 
-            if (leftChildIndex < size && this.heap.get(leftChildIndex) < this.heap.get(minElementIndex)) {
+            if (leftChildIndex < size && this.heap.get(leftChildIndex).getPriority() < this.heap.get(minElementIndex).getPriority()) {
                 minElementIndex = leftChildIndex;
             }
 
-            if (rightChildIndex < size && this.heap.get(rightChildIndex) < this.heap.get(minElementIndex)) {
+            if (rightChildIndex < size && this.heap.get(rightChildIndex).getPriority() < this.heap.get(minElementIndex).getPriority()) {
                 minElementIndex = rightChildIndex;
             }
 

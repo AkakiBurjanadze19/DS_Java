c76042008e02bb8a3f368d5bbada780d2c2dc3de
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-06 18:18:36 +0400

    add MinHeapUsage.java

---
+++ CUSTOM_DATA_STRUCTURES/NON_LINEAR/Heap/MinHeapUsage.java
@@ -0,0 +1,23 @@
+package CUSTOM_DATA_STRUCTURES.NON_LINEAR.Heap;
+
+import CUSTOM_DATA_STRUCTURES.NON_LINEAR.Heap.MinHeap.MinHeap;
+
+public class MinHeapUsage {
+    public static void main(String[] args) {
+        MinHeap heap = new MinHeap();
+
+        heap.insert(20);
+        heap.insert(15);
+        heap.insert(10);
+        heap.insert(5);
+
+//        System.out.println("heap size: " + heap.size());
+//        System.out.println("extracted min: " + heap.extractMin());
+//        System.out.println("heap size: " + heap.size());
+//        System.out.println("min element: " + heap.getMin());
+
+        while (!heap.isEmpty()) {
+            System.out.println(heap.extractMin());
+        }
+    }
+}

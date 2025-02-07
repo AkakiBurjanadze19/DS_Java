2f3b8197e39066ff4889d8c6c3d2389d39f88dab
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-02-07 16:07:44 +0400

    add BinarySearchTreeNode.java

---
+++ CUSTOM_DATA_STRUCTURES/NON_LINEAR/BinarySearchTree/BinarySearchTreeNode.java
@@ -0,0 +1,13 @@
+package CUSTOM_DATA_STRUCTURES.NON_LINEAR.BinarySearchTree;
+
+public class BinarySearchTreeNode {
+    public int data;
+    public BinarySearchTreeNode left;
+    public BinarySearchTreeNode right;
+
+    public BinarySearchTreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
